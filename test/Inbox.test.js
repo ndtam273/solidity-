f0d67bd50aa554aa383bab797429d0f7d63090a9
tam nguyen <ndtam.273@gmail.com>
2019-11-13 17:58:44 +0700

    add

---
+++ test/Inbox.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+// Get a list of all accounts
+accounts = await web3.eth.getAccounts()
+// Use one of those accounts to deploy
+// the contracts
+inbox = await new web3.eth.Contract(JSON.parse(interface))
+.deploy({data: bytecode, arguments: ['Hi there!']})
+.send({from: accounts[0], gas: '1000000'});
+
+inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () =>{
+     assert.ok(inbox.options.address);
+  });
+});
